83518a19aef457063696bcc8f8a2144f9fa81355
DennisRono <dennisrkibet@gmail.com>
2023-10-06 19:05:12 +0300

    views routing and horizontal pageviews

---
+++ mobile/lib/screens/ChatHomeScreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatHomeScreen extends StatelessWidget {
+  const ChatHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(
+      child: Center(
+        child: Text('Chat HomePage'),
+      ),
+    );
+  }
+}
---
+++ mobile/lib/screens/HomePage.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'ChatHomeScreen.dart';
+import 'MyAccount.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final PageController _pageController = PageController();
+  int _currentPageIndex = 0;
+
+  void _onPageChanged(int index) {
+    setState(() {
+      _currentPageIndex = index;
+    });
+  }
+
+  void _onBottomNavItemTapped(int index) {
+    setState(() {
+      _currentPageIndex = index;
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: _onPageChanged,
+        children: const [
+          ChatHomeScreen(),
+          MyAccount(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        onTap: _onBottomNavItemTapped,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        selectedFontSize: 10.0,
+        unselectedFontSize: 10.0,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Chats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+}
