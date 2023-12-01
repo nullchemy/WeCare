5f642a33c6900bccbedee7b94b282b40ba0fc402
DennisRono <dennisrkibet@gmail.com>
2023-12-01 23:02:41 +0300

    path side left

--- backend/server/chat/chatbot.py
+++ backend/server/chat/chatbot.py
@@ -50,13 +50,13 @@ def printmd(string):
 
 def load_tokenizer_and_model(model="microsoft/DialoGPT-large"):
   global tokenizer
-  tokenizer = AutoTokenizer.from_pretrained(model)
+  tokenizer = AutoTokenizer.from_pretrained(model, padding_side='left')
   model = AutoModelForCausalLM.from_pretrained(model)
   return tokenizer, model
 
 def load_suicide_tokenizer_and_model(tokenizer="google/electra-base-discriminator", model="Models/electra"):
   global suicide_tokenizer, suicide_model
-  suicide_tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+  suicide_tokenizer = AutoTokenizer.from_pretrained(tokenizer, padding_side='left')
   suicide_model = AutoModelForSequenceClassification.from_pretrained(model)
   return suicide_tokenizer, suicide_model
 
