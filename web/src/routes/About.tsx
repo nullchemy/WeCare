036d31ae0080e75c11cf052cdbe2b3794e3e725e
DennisRono <dennisrkibet@gmail.com>
2024-03-15 13:10:28 +0300

    changed redis connect mode

--- web/src/routes/About.tsx
+++ web/src/routes/About.tsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import '../styles/css/about.css'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
 
 const About = () => {
-  return <div>About</div>
+  return (
+    <div className="about">
+      <Header />
+      <div className="about_container">
+        <div className="about_wrapper">
+          <h2 className="about_header_title">About WeCare</h2>
+          <p className="about_paragraphs">
+            Wecare is a chatbot application for suicidal detection and support.
+          </p>
+          <p className="about_paragraphs">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem
+            impedit, quo harum et necessitatibus eaque aspernatur qui, quisquam
+            reprehenderit nam. Minus, ratione. Inventore officiis, accusamus
+            quas unde cumque voluptatibus!
+          </p>
+          <p className="about_paragraphs">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus autem
+            impedit, quo harum et necessitatibus eaque aspernatur qui, quisquam
+            reprehenderit nam. Minus, ratione. Inventore officiis, accusamus
+            quas unde cumque voluptatibus!
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default About
